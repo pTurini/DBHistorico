3a69b9865d8d8283360973f46c9d5e94cc387ff7
Pedro Turini <pedro.turini@usp.br>
2023-03-22 11:50:08 -0300

    Rascunho inicial de scraping com SPARQL, Python e JSON

--- WebScraper/scraper_wd.py
+++ WebScraper/scraper_wd.py
@@ -1,1 +1,21 @@
 #usando wikidata, parece ser mais fácil
+import requests
+import pandas as pd
+from collections import OrderedDict
+
+url = 'https://query.wikidata.org/sparql'
+query="""
+SELECT 
+  ?pessoa ?pessoaLabel ?nascimento ?morte ?nacionalidade
+WHERE {
+  ?pessoa wdt:P31 wd:Q5. #pessoas 
+  ?pessoa wdt:P19 wd:Q28. #pessoas hungaras
+  ?pessoa wdt:P569 ?nascimento.
+  ?pessoa wdt:P570 ?morte.
+  ?pessoa wdt:P19 ?nacionalidade.
+  
+  SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
+  """
+
+r = requests.get(url, params = {'format': 'json', 'query': query})
+data = r.json()
